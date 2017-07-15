0c576e7b9e46b8c1907725f089c876cfe758157e
marcjwilliams1 <marc.williams.13@ucl.ac.uk>
2017-07-15 17:44:41 +0100

    Changed ggplotting colours again

--- src/ggplotting.jl
+++ src/ggplotting.jl
@@ -8,9 +8,9 @@ function ggplot(dp, directory)
   library(ggplot2)
   library(cowplot)
   g <- ggplot(DFres, aes(x = x, y = mean)) +
-  geom_histogram(data = DF, aes(x = VAF, y = ..density..), bins = 100, alpha = 0.8, col = "lightgrey") +
-  geom_line(col = "lightblue4", size = 0.8, alpha = 0.8) +
-  geom_ribbon(aes(ymin = lq, ymax = uq), col = "lightblue4", fill = "lightblue3", alpha = 0.4) +
+  geom_histogram(data = DF, aes(x = VAF, y = ..density..), bins = 100,   fill = "gray68") +
+  geom_line(col = "lightblue4", size = 1.0, alpha = 1.0) +
+  geom_ribbon(aes(ymin = lq, ymax = uq), size = 0.2, col = "lightblue4", fill = "lightblue3", alpha = 0.7) +
   xlab("VAF") +
   ylab("Density")
 
