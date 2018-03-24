41075e8d49b34ff9c930856c1752b8ebbbb19fe1
Marc <marc.williams.13@ucl.ac.uk>
2018-03-24 18:14:25 +0000

    Some small changes

--- src/DPclustering.jl
+++ src/DPclustering.jl
@@ -1,11 +1,9 @@
 module DPclustering
 
 # packages
-#using RCall
 using DataFrames
 using ProgressMeter
 using KernelDensity
-#using CancerSeqSim
 using Gadfly
 using Distributions
 using Colors
@@ -20,7 +18,6 @@ show
 include("types.jl")
 include("clustering.jl")
 include("plotting.jl")
-#include("ggplotting.jl")
 include("util.jl")
 
 end # module
